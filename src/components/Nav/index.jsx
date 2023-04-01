61990314bc978604a93f0c6d3379b799a26c31d7
rMaloberti <r.maloberti@yahoo.com>
2023-04-01 12:26:33 +0200

    [header] create unauthenticated version of the component

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,21 @@
+import argentBankLogo from '../../assets/argentBankLogo.png';
+import { Link } from 'react-router-dom';
+import './index.css';
+
+const Nav = () => {
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img className="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <Link className="main-nav-item" to="/sign-in">
+          <i className="fa fa-user-circle"></i> Sign In
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Nav from './components/Nav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router></Router>
+    <Router>
+      <Nav />
+    </Router>
   </React.StrictMode>
 );
 
