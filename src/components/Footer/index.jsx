091f61cf501a5453f0bbffccb7d24d4c69dc3d14
rMaloberti <r.maloberti@yahoo.com>
2023-04-01 12:33:55 +0200

    [Footer] create component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <p class="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Nav />
+      <Routes></Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
